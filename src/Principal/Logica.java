bbb81fed21e4dcea851b84c43c64c9c0bff64237
YamilHasan <126710059+YamilHasan@users.noreply.github.com>
2023-03-18 19:35:08 -0300

    Update Logica.java

--- src/Principal/Logica.java
+++ src/Principal/Logica.java
@@ -6,11 +6,7 @@ public class Logica {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		//54654654654654654654
-		for(int i = 1 ; i > 2 ; i++){
-
-		}
+		System.out.print("jjjjjjjjjjjjjj");
 	}
 
 }
